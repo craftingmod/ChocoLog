b09fea686708f8654ff919b733176e5727303e10
Craftingmod <alyac1234@gmail.com>
2019-02-08 10:25:49 +0900

    Dongbek Flower Sample

--- src/launch.ts
+++ src/launch.ts
@@ -1,19 +1,37 @@
 import wcwidth from "wcwidth"
 import { dongbak } from "./dongbak"
-import { substringMono, unicodeLn } from "./log"
+import { substrMono, unicodeLn } from "./log"
 
 console.log("Hello World")
 
 const dongbaks = dongbak.split("\n")
+const splits:string[] = []
+const test = substrMono("늬 집은 이런거 없제잉", 3, 9)
+console.log(test)
+for (const str of dongbaks) {
+    const column = process.stdout.columns
+    for (let i = 0; i < wcwidth(str); i += column) {
+        const part = substrMono(str, i, column - 1) + "|"
+        splits.push(part)
+    }
+}
+console.log(splits.join("\n"))
+/*
 for (let i = 0; i < dongbaks.length; i += 1) {
     const ln = unicodeLn(dongbaks[i])
     let k = 0
-    while (k < ln) {
-        const part = substringMono(dongbaks[i], k, Math.min(ln, k + process.stdout.columns))
-        console.log(part)
-        k += unicodeLn(part)
+    let u = 0
+    let part:string
+    while (k + u < ln) {
+        part = substringMono(dongbaks[i], k, Math.min(ln, k + u + process.stdout.columns))
+        u = unicodeLn(part)
     }
+    k += u
+    u = 0
+    splits.push(part)
+    console.log(part)
 }
+*/
 
 function logUnicode() {
     for (let i = 0; i <= 255; i += 1) {
--- src/log.ts
+++ src/log.ts
@@ -18,27 +18,34 @@ export function unicodeLn(text:string) {
     }
     return ln
 }
-export function substringMono(text:string, start:number, end:number, floor = true) {
+export function substrMono(text:string, start:number, length:number) {
     if (text.indexOf("\n") >= 0) {
         throw new Error("Line seperator didn't allowed.")
     }
-    let out:string = ""
-    let ln = 0
-    for (let i = 0; i < text.length; i += 1) {
-        const char = text.charAt(i)
-        const oldLn = ln
-        if (char === "\t") {
-            ln = Math.ceil((ln + 1) / 4) * 4
+    // https://stackoverflow.com/a/34717402
+    const charsets = [...text]
+    // https://stackoverflow.com/questions/45803829/memory-overhead-of-typed-arrays-vs-strings
+    // tslint:disable-next-line
+    let out = new String()
+    const tab = "\t"
+    let totalLn = 0
+    let untilLn = 0
+    for (let i = 0; i < charsets.length; i += 1) {
+        const char = text[i]
+        const ln = wcwidth(char)
+        if (char === tab) {
+            totalLn = Math.ceil((totalLn + 1) / 4) * 4
         } else {
-            ln += wcwidth(char)
+            totalLn += ln
         }
-        if (floor && ln > end) {
-            break
+        if (i < start) {
+            continue
+        } else if (untilLn === 0) {
+            untilLn = totalLn - ln + length
         }
-        if ((floor && start >= oldLn) || (!floor && start >= ln)) {
+        if (totalLn <= untilLn) {
             out += char
-        }
-        if (!floor && ln >= end) {
+        } else {
             break
         }
     }
